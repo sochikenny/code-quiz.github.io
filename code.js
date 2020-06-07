6e5bb081d906f73613bbfb7c841494ac8bee85c9
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2020-06-06 22:07:45 -0400

    switch everything completely

--- code.js
+++ code.js
@@ -1,117 +1,35 @@
-var quizquestions = [
-    {
-      question: "Who was the richest person in the history of the world?",
+const correctAnswers = ['A', 'B', 'B', 'A'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result')
 
-       picks: ["JD Rockefellar",
-                "Mansa Musa",
-                "Jeff Bezos"],
+form.addEventListener('submit', e => {
+    e.preventDefault();
 
-      correctAnswer: "Mansa Musa"
-    },
-    
-    {
-      question: "Which country has the second largest black population in the world?",
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
 
-        picks: ["Nigeria",
-                "South Africa",
-                "Brazil"],
-
-      correctAnswer: "Brazil"
-    },
-
-    {
-      question: "What college did Michelle Obama attend for her undergrad?",
-
-        picks: ["Harvard",
-                 "Yale",
-                 "Princeton"],
-
-      correctAnswer: "Princeton"
-    }
-  ];
-
-  //placing questions on page
-$(document).ready(function () {
-  for (var i = 0; i < quizquestions.length; i++){
-    var questiondiv = $("<div>").text(quizquestions[i].question);
-    var pickA = $("<button>").text(quizquestions[i].picks[0]);
-    var pickB = $("<button>").text(quizquestions[i].picks[1]);
-    var pickC = $("<button>").text(quizquestions[i].picks[2]);
-    $(".container").append(questiondiv, pickA, pickB, pickC);
-
-    var count = 0;
-    var userResponse = quizquestions[i].picks[i];
-      if (userResponse === quizquestions[i].correctAnswer){
-        count++
-         
-      }
-    
-  
-  }
-
-//On click event: when you click start, the timer starts running
-  $(".start").on("click", function() {
-  
-      function setTime(){
-        var timeEl = $(".timer");
-        var secondsleft = 30;
-        var timerinterval = setInterval(function(){
-        secondsleft--;
-        timeEl.text(secondsleft + " seconds left ");
-        if (secondsleft === 0){
-          clearInterval(timerinterval);
+    //check answers
+    userAnswers.forEach((answer, index) => {
+        if(answer === correctAnswers[index]){
+            score += 25;
         }
-      }, 1000);
-  
-    }
-    setTime();
+    });
+    
+    //show result on page
+    scrollTo(0,0);
+    result.querySelector('span').textContent = `${score}%`
+    result.classList.remove('d-none');
 
-
-    //need to put renderScore function here
-  });
- 
- $(".submit").on("click", function(){
- 
-  renderScore();
-
- });
-  //for (var i = 0; i < quizquestions.length; i++){
-    //$("button").on("click", function(){
-      //var count = 0;
-      //var userResponse
-    //});
-  //function renderScore(){
-    //var count = 0;
-    //var userResponse = quizquestions[i].picks[i];
-    //if (userResponse === quizquestions[i].correctAnswer[i]){
-      //count++
-      //alert(count); 
-    //}
-  //}
-//}
-
-  //create an on click event for submit that will give score to user (basically a submit onclick and inside there will be the renderScore function)
-  //creating a new function called renderScore (will show score)
-  //clicking answer choices will be involved and tallying the score
-
-
-   
-
-
-
-
-
-
-//var score = 0;
-//function checkAnswer(answer){
-    //if (quizquestions[i].correct === answer){
-       // score++;
-    //}
-//}
-
-//function result(){
-  
-//}
+    //score animation
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if(output === score){
+            clearInterval(timer);
+        }else{
+            output++;
+        }
+    }, 10);
 
 
 });
